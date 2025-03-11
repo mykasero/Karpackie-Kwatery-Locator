88fca84cb3db9bb712fb321274bf9590381083bf
MyKasero <mykasero20@gmail.com>
2025-03-11 23:46:47 +0100

    added simple barebone views for displaying the templates

--- core/views.py
+++ core/views.py
@@ -6,3 +6,15 @@ def home(request):
     context = TestModel.objects.all().values()
     
     return render(request,"core/home.html", {'context':context})
+
+def apartments(request):
+    return render(request,"core/apartments.html")
+
+def gallery(request):
+    return render(request,"core/gallery.html")
+
+def contact(request):
+    return render(request,"core/contact.html")
+
+def admin_page(request):
+    return render(request,"core/admin_page.html")
