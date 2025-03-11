a7dd39d789f64f2e885a33c51f1ce519352060b6
MyKasero <mykasero20@gmail.com>
2025-03-11 23:46:27 +0100

    adding the urls for the rest of the views (more TBD)

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("kwatery/", views.apartments, name="apartments"),
+    path("galeria/", views.gallery, name="gallery"),
+    path("kontakt/", views.contact, name="contact"),
+    path("administracja/", views.admin_page, name="admin_page"),
 ]
