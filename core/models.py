f656bb4a568d252419e5422ac717c134416c261d
MyKasero <mykasero20@gmail.com>
2025-03-21 00:25:30 +0100

    added the appartments model and a linked model for appartment photos

--- core/models.py
+++ core/models.py
@@ -1,8 +1,24 @@
 from django.db import models
-
+from django.conf import settings
+from django.utils import timezone
 # Create your models here.
 
 class TestModel(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(db_column="Test name")
     test_text = models.TextField(db_column="test text")
+    
+class AppartmentsModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(db_column="Appartment name")
+    address = models.CharField(db_column="Appartment address")
+    city = models.CharField(db_column="City")
+    description = models.TextField(db_column="Appartment description")
+    extra_desc = models.TextField(db_column="Additional description", default='', blank=True)
+    uploaded_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+class AppartmentsPhotosModel(models.Model):
+    appartment = models.ForeignKey(AppartmentsModel, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='appartments_images/')
