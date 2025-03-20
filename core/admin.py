a49160aaccf7d9e961afebcb64ac332c2a589c83
MyKasero <mykasero20@gmail.com>
2025-03-21 00:27:16 +0100

    registered the new models in the admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import TestModel
+from .models import TestModel, AppartmentsModel, AppartmentsPhotosModel
 # Register your models here.
 
 @admin.register(TestModel)
 class TestModelAdmin(admin.ModelAdmin):
     ordering = ['id']
     list_display=('id','name','test_text')
+    
+@admin.register(AppartmentsModel)
+class AppartmentsModelAdmin(admin.ModelAdmin):
+    ordering = ['id']
+    list_display=('id','name','address', 'description', 'extra_desc','uploaded_on','updated_on','uploaded_by',)
+    
+@admin.register(AppartmentsPhotosModel)
+class AppartmentsPhotosModelAdmin(admin.ModelAdmin):
+    list_display=('id','image',)
