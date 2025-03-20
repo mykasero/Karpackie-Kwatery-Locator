61288ed8f4b3981be46f10728e536a1e45ca5895
MyKasero <mykasero20@gmail.com>
2025-03-21 00:22:47 +0100

    added script for the toggling of the second level dropdown list

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -55,5 +55,36 @@ window.addEventListener('DOMContentLoaded', event => {
     new SimpleLightbox({
         elements: '#portfolio a.portfolio-box'
     });
+    
+    // Custom JavaScript for nested dropdowns
+    const dropdownSubmenus = document.querySelectorAll('.dropdown-submenu');
 
+    dropdownSubmenus.forEach(function (submenu) {
+        const dropdownToggle = submenu.querySelector('.dropdown-toggle');
+
+        dropdownToggle.addEventListener('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            // Close other open submenus
+            dropdownSubmenus.forEach(function (otherSubmenu) {
+                if (otherSubmenu !== submenu) {
+                    otherSubmenu.querySelector('.dropdown-menu').classList.remove('show');
+                }
+            });
+
+            // Toggle the current submenu
+            const dropdownMenu = submenu.querySelector('.dropdown-menu');
+            dropdownMenu.classList.toggle('show');
+        });
+    });
+
+    // Close dropdowns when clicking outside
+    document.addEventListener('click', function () {
+        dropdownSubmenus.forEach(function (submenu) {
+            submenu.querySelector('.dropdown-menu').classList.remove('show');
+        });
+    });
 });
+
+
