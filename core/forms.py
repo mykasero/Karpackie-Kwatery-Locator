727cfbfcafb56941d873b09be533e45569ac4f5e
MyKasero <mykasero20@gmail.com>
2025-04-03 22:44:59 +0200

    added login and register forms for admins

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 from .models import AppartmentsModel, AppartmentsPhotosModel, HomepageCounters
 from django.forms.widgets import ClearableFileInput
+from django.contrib.auth.forms import UserCreationForm
+from django.utils.safestring import mark_safe
+from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
+
+
+
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 class MultipleFileInput(forms.ClearableFileInput):
     allow_multiple_selected = True
@@ -48,3 +58,94 @@ class CountersForm(forms.ModelForm):
             'locations_amount' : "Ilość miejscowości",
             'clients_amount' : 'Ilość klientów',
         }
+        
+# Login Form
+class LoginForm(forms.Form):
+    login = forms.CharField(label = '', max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Login'}))
+    haslo = forms.CharField(label = '', widget=forms.PasswordInput(attrs={'placeholder': 'Haslo'}), max_length=40)
+    
+# Register form
+class RegisterForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        # override Djangos default label
+        self.fields['password1'].label = mark_safe('<strong>Hasło</strong>')
+        self.fields['password2'].label = mark_safe('<strong>Potwierdź hasło</strong>')
+        # override Djangos default helptext
+        self.fields['password1'].help_text = mark_safe('<ul><li>Minimum 8 znaków.</li><li>Nie może być podobne do loginu.</li><li>Nie może być całkowicie złożone z cyfr.</li></ul>')
+        self.fields['password2'].help_text = ' '
+    
+    # Add an access_code field for register form
+    access_code = forms.CharField(
+        label=mark_safe("<strong>Kod Dostepu</strong>"),
+        help_text=" ",
+    )
+    usable_password = None
+    class Meta:
+        model = User
+        fields = ['username','password1','password2','access_code']
+
+        labels={
+            'username' : mark_safe('<strong>Nazwa użytkownika</strong>'),
+        }
+        help_texts= {
+            'username' : 'Max długość 50 znaków. Dozwolone litery, cyfry i symbole @/./+/-/_',
+        }
+    
+    # Create validation checks
+    def clean(self):
+        cleaned_data = super().clean()
+        
+        username = cleaned_data.get('username')
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+        access_code = cleaned_data.get('access_code')
+        
+        # If passwords don't match throw error
+        if password1 != password2:
+            self.add_error(None,forms.ValidationError(_("Hasła nie są takie same"),
+                                  code="invalid",
+                                  ))
+        # If any of the passwords ( or both ) are empty, throw error
+        if password1 == "" or password2 == "" or (password1 == "" and password2 == ""):
+            self.add_error(None,forms.ValidationError(_("Pola haseł nie mogą być puste"),
+                                  code="invalid",
+                                  ))
+        # If password shorter than 8 chars, throw error
+        if len(password1) < 8:
+            self.add_error(None,forms.ValidationError(_("Hasło za krótkie, minimalna długość 8 znaków"),
+                                  code="invalid",
+                                  ))
+        
+        # If username is empty, throw error
+        if username is None:
+            self.add_error(None,   
+                           forms.ValidationError(
+                               _("Użytkownik o takiej nazwie już istnieje"),
+                                code="invalid",
+                                )
+                           )
+        # If username is longer than 50 chars, throw error
+        elif len(username) > 50:
+            self.add_error(None,forms.ValidationError(_("Nazwa za długa, maksymalna długość - 50 znaków"),
+                                  code="invalid",
+                                  ))
+        
+        # If access_code is wrong, throw error
+        if access_code not in [env("ADMIN_REGISTER_CODE")]:
+            self.add_error(None,   
+                           forms.ValidationError(
+                               _("Podano zły kod dostępu"),
+                                code="invalid",
+                                )
+                           )
+        # If password is only numeric, throw error
+        if password1.isdigit():
+            self.add_error(None,   
+                           forms.ValidationError(
+                               _("Hasło nie może być złożone z samych cyfr"),
+                                code="invalid",
+                                )
+                           )
+        
+        return cleaned_data
